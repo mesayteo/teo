6a15700f3e14e81541be12dc08fb7cb81a5b016e
Teo <hllr@teo.fm>
2016-06-23 05:07:28 -0700

    Center card, Toggle cards
    Center the main section, and add jQuery logic for toggling cards.

---
+++ custom.js
@@ -0,0 +1,31 @@
+$('.ui.embed').embed({
+	autoplay: false,
+	hd: true
+});
+
+//hide the social card
+
+$('.card').not('.active').hide();
+
+// utility functions
+function toggle_cards(){
+
+	$('.card:visible').transition({
+		animation: 'fly down out',
+		duration: 150,
+		onComplete: function(){
+			$('.card').toggleClass('active');
+			$('.active').transition('fly down in', 150);
+			
+		}
+	})
+}
+
+
+// on @mesayteo click
+	$('a.toggle').click(function(event){
+		
+		event.preventDefault();
+		// $(this).children('i').toggleClass('blue');
+		toggle_cards();
+	}); // end playlist button
